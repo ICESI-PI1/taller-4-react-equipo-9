8e05ad79d8cda5fae55f29ba09b7f673719f6f08
JesusGarce22 <jesus02garces@gmail.com>
2023-10-30 23:49:24 -0500

    delete

--- BooksLibrary/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:8080', })
-    
-    localStorage.getItem('token')&&(instance.defaults.headers.common['Authorization']= localStorage.getItem('token'))
-
-    
-export default instance;
